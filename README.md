3215bb99a25e7ba6eadd62dd6d5f63ad816eb273
Sunny Kim <sungunkim367@gmail.com>
2021-03-14 22:21:56 +0900

    [ADD] First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+ # Popular Python Web Frameworks 
+A list of popular Python web frameworks ranked by the number of GitHub stars, automatically updated every week.
+
+![update](https://github.com/sunnysid3up/python-web-frameworks/workflows/update/badge.svg) Last update: 03/14/2021, 13:21:24 (UTC)
+
+| Name          | Description          | Stars                     | Forks          | Issues               | First Commit        | Last Commit         |
+|---------------|----------------------|---------------------------|----------------|----------------------|---------------------|---------------------|
+| [django](https://github.com/django/django) | The Web framework for perfectionists with deadlines. | 56164 | 24073 | 163 | 2012 | 2021-03-14 |
+| [flask](https://github.com/pallets/flask) | The Python micro framework for building web applications. | 54199 | 14161 | 21 | 2010 | 2021-03-14 |
+| [fastapi](https://github.com/tiangolo/fastapi) | FastAPI framework, high performance, easy to learn, fast to code, ready for production | 28465 | 1947 | 737 | 2018 | 2021-03-14 |
+| [tornado](https://github.com/tornadoweb/tornado) | Tornado is a Python web framework and asynchronous networking library, originally developed at FriendFeed. | 19857 | 5338 | 222 | 2009 | 2021-03-13 |
+| [sanic](https://github.com/sanic-org/sanic) | Async Python 3.7+ web server/framework. Build fast. Run fast. | 14674 | 1319 | 33 | 2016 | 2021-03-14 |
+| [aiohttp](https://github.com/aio-libs/aiohttp) | Asynchronous HTTP client/server framework for asyncio and Python | 10943 | 1565 | 359 | 2013 | 2021-03-14 |
+
+## Contribute 
+
+Create an issue or pull request if you would like to add more frameworks :)
---
+++ main.py
@@ -0,0 +1,85 @@
+import os
+from dataclasses import dataclass
+from datetime import datetime, timezone
+from typing import List
+import requests
+
+
+@dataclass
+class Repo:
+    name: str
+    description: str
+    url: str
+    stars: str
+    forks: str
+    issues: str
+    created_at: str
+    updated_at: str
+
+
+def main():
+    result = []
+    repos = file_read()
+    headers = {
+        "Authorization": f"token {os.environ.get('GTOKEN')}",
+        "Content-Type": "application/json"
+    }
+    for repo in repos:
+        url = "https://api.github.com/repos/" + repo
+        resp = requests.get(url, headers=headers)
+        status_code, body = resp.status_code, resp.json()
+        if status_code != 200:
+            print(f"{status_code} : {body}")
+        else:
+            result.append(format_body(body))
+    file_write(sort_by_stars(result))
+    return
+
+
+def format_body(body):
+    data = Repo(**{
+        "name": body["name"],
+        "description": body["description"],
+        "url": body["html_url"],
+        "stars": body["stargazers_count"],
+        "forks": body["forks"],
+        "issues": body["open_issues"],
+        "created_at": body["created_at"],
+        "updated_at": body["updated_at"]
+    })
+    print(data)
+    return data
+
+
+def sort_by_stars(repos: List[Repo]):
+    def by_stars(elem):
+        return elem.stars
+
+    sorted_repos = sorted(repos, key=by_stars, reverse=True)
+    print("Sorted repos.")
+    return sorted_repos
+
+
+def file_read():
+    with open("repos.txt", "r") as reader:
+        repos = reader.read().splitlines()
+        print("Accessed repos.txt")
+        return repos
+
+
+def file_write(repos):
+    with open("README.md", "w") as writer:
+        body = f""" # Popular Python Web Frameworks \nA list of popular Python web frameworks ranked by the number of GitHub stars, automatically updated every week.\n\n![update](https://github.com/sunnysid3up/python-web-frameworks/workflows/update/badge.svg) Last update: {datetime.now(tz=timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")} (UTC)
+
+| Name          | Description          | Stars                     | Forks          | Issues               | First Commit        | Last Commit         |
+|---------------|----------------------|---------------------------|----------------|----------------------|---------------------|---------------------|"""
+        for repo in repos:
+            body += f"\n| [{repo.name}]({repo.url}) | {repo.description.replace(' |', '.')} | {repo.stars} | {repo.forks} | {repo.issues} | {repo.created_at[:4]} | {repo.updated_at[:-10]} |"
+        body += "\n\n## Contribute \n\nCreate an issue or pull request if you would like to add more frameworks :)"
+        writer.write(body)
+
+    print("Updated README.")
+
+
+if __name__ == "__main__":
+    main()
