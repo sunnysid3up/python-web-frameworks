7d97c226970f6d9233df9941e09b836967eca754
Sunny Kim <sungunkim367@gmail.com>
2021-04-01 14:37:21 +0900

    [ADD] Status badge during auto-update

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def file_read():
 
 def file_write(repos):
     with open("README.md", "w") as writer:
-        body = f"""# Popular Python Web Frameworks \nA list of popular Python web frameworks ranked by the number of GitHub stars, automatically updated every week.\n\nLast update: {datetime.now(tz=timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")} (UTC)
+        body = f"""# Top Python Web Frameworks\n\n[![update](https://github.com/sunnysid3up/python-web-frameworks/actions/workflows/update.yml/badge.svg)](https://github.com/sunnysid3up/python-web-frameworks/actions/workflows/update.yml)\n\nA list of popular Python web frameworks ranked by the number of GitHub stars, automatically updated every week.\n\nLast update: {datetime.now(tz=timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")} (UTC)
 
 | Name          | Description          | Stars                     | Forks          | Issues               | First Commit        | Last Commit         |
 |---------------|----------------------|---------------------------|----------------|----------------------|---------------------|---------------------|"""
